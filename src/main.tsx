4a0e9b489ab36afc8254d11a200af9ae0f989ce0
PRAISE FM AUSTRALIA <praisefmaustralia@gmail.com>
2026-03-22 17:03:42 +0000

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
 
-const rootElement = document.getElementById('root')
-
-if (!rootElement) {
-  throw new Error('Root element not found')
-}
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
