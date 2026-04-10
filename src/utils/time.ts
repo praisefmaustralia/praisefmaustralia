c4805ae8545d38804afb3391f44ed3f4867931d1
PRAISE FM AUSTRALIA <praisefmaustralia@gmail.com>
2026-04-10 12:54:52 -0300

    update

--- src/utils/time.ts
+++
@@ -1,7 +0,0 @@
-export const formatToAmPm = (time: string): string => {
-  const [hours, minutes] = time.split(':').map(Number);
-  const period = hours >= 12 ? 'PM' : 'AM';
-  const hour12 = hours % 12 || 12;
-
-  return `${hour12}:${minutes.toString().padStart(2, '0')} ${period}`;
-};
