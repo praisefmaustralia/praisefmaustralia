65b7c298c81811b0b14dc48b6253c9b6455907e3
PRAISE FM AUSTRALIA <praisefmaustralia@gmail.com>
2026-06-19 04:35:41 +0000

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
+declare module 'virtual:pwa-register' {
+  export function registerSW(options?: { immediate?: boolean }): void
+}
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
