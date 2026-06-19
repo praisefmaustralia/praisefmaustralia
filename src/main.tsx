26153809b6952cb993fda95b16b3338221b7bf3c
PRAISE FM AUSTRALIA <praisefmaustralia@gmail.com>
2026-06-19 05:40:56 +0000

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
-declare module 'virtual:pwa-register' {
-  export function registerSW(options?: { immediate?: boolean }): void
-}
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-;(async () => {
-  const { registerSW } = await import('virtual:pwa-register')
-  registerSW({ immediate: true })
-})()
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
