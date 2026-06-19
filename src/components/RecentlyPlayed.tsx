dee7365f9cc7019fb6280685da393a7aabf37be1
PRAISE FM AUSTRALIA <praisefmaustralia@gmail.com>
2026-06-19 14:47:30 +0000

    update

--- src/components/RecentlyPlayed.tsx
+++ src/components/RecentlyPlayed.tsx
@@ -10,9 +10,18 @@ interface RecentlyPlayedProps {
   tracks: Track[];
 }
 
-export default function RecentlyPlayed({ tracks }: RecentlyPlayedProps) {
-  if (!tracks || tracks.length === 0) {
-    return null;
+export default function RecentlyPlayed({
+  tracks,
+}: RecentlyPlayedProps) {
+  const displayTracks = [...tracks];
+
+  while (displayTracks.length < 4) {
+    displayTracks.push({
+      title: "",
+      artist: "",
+      artwork:
+        "https://res.cloudinary.com/ddhu86ukg/image/upload/v1774221235/SVGAUS_qmzryk.png",
+    });
   }
 
   return (
@@ -23,25 +32,21 @@ export default function RecentlyPlayed({ tracks }: RecentlyPlayedProps) {
       </div>
 
       <div className="recently-played__grid">
-        {tracks.map((track, index) => (
+        {displayTracks.slice(0, 4).map((track, index) => (
           <article
             className="recently-played__card"
             key={`${track.artist}-${track.title}-${index}`}
           >
             <div className="recently-played__art">
-              {track.artwork || track.image ? (
-                <img
-                  src={track.artwork || track.image}
-                  alt={`${track.title} by ${track.artist}`}
-                />
-              ) : (
-                <span>{index + 1}</span>
-              )}
+              <img
+                src={track.artwork || track.image}
+                alt={track.title}
+              />
             </div>
 
             <div>
-              <h3>{track.title}</h3>
-              <p>{track.artist}</p>
+              <h3>{track.title || "Praise FM Australia"}</h3>
+              <p>{track.artist || "The Global Worship Network"}</p>
             </div>
           </article>
         ))}
