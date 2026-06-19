e57f767a52ec608f9b866eefaba4d0089b8fd8ad
PRAISE FM AUSTRALIA <praisefmaustralia@gmail.com>
2026-06-19 04:32:53 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'icon-192.png', 'icon-512.png'],
+      manifest: {
+        name: 'Praise FM Australia',
+        short_name: 'Praise FM',
+        description: '24/7 Worship & Christian Music from Australia.',
+        theme_color: '#ff6600',
+        background_color: '#121212',
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: '/index.html',
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}'],
+      },
+    }),
+  ],
 })
